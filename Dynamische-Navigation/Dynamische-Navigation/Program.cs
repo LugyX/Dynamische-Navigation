13dc75df0ccb94254c52c3d23f2875c5b6407954
Patrick Hahn <patricksilashahn@gmx.net>
2013-11-25 21:04:51 +0100

    Fixed merge conflicts

--- Dynamische-Navigation/Dynamische-Navigation/Program.cs
+++ Dynamische-Navigation/Dynamische-Navigation/Program.cs
@@ -9,12 +9,8 @@ namespace Dynamische_Navigation
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Hallo Genossen und Genossinen");
-=======
             Console.WriteLine("Hallo Genossen und Genossinen: by patrick246");
 			Console.ReadKey();
->>>>>>> refs/heads/Hello-World-message-edit
         }
     }
 }
