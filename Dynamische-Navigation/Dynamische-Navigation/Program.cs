391b2a2cc232267695ea257008650a3427bf1ac8
Patrick Hahn <patricksilashahn@gmx.net>
2013-11-25 21:02:15 +0100

    Commit by patrick246

---
+++ Dynamische-Navigation/Dynamische-Navigation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dynamische_Navigation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Genossen und Genossinen: by patrick246");
+			Console.ReadKey();
+        }
+    }
+}
